38678e3505afdfaecea2b65dda26632b6b56f89c
ttmuangkeo <tim.muangkeo@gmail.com>
2017-04-24 23:38:44 -0700

    solutions

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var moment = require('moment');
 var rowdy = require('rowdy-logger');
 var app = express();
 
+
 rowdy.begin(app);
 
 app.set('view engine', 'ejs');
@@ -17,19 +18,19 @@ app.use(express.static(__dirname + '/public/'));
 
 // middleware that allows us to access the 'moment' library in every EJS view
 app.use(function(req, res, next) {
-  res.locals.moment = moment;
-  next();
+    res.locals.moment = moment;
+    next();
 });
 
 // GET / - display all posts and their authors
 app.get('/', function(req, res) {
-  db.post.findAll({
-    include: [db.author]
-  }).then(function(posts) {
-    res.render('main/index', { posts: posts });
-  }).catch(function(error) {
-    res.status(400).render('main/404');
-  });
+    db.post.findAll({
+        include: [db.author]
+    }).then(function(posts) {
+        res.render('main/index', { posts: posts });
+    }).catch(function(error) {
+        res.status(400).render('main/404');
+    });
 });
 
 // bring in authors and posts controllers
@@ -37,7 +38,7 @@ app.use('/authors', require('./controllers/authors'));
 app.use('/posts', require('./controllers/posts'));
 
 var server = app.listen(process.env.PORT || 3000, function() {
-  rowdy.print();
+    rowdy.print();
 });
 
 module.exports = server;
