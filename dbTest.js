18dee3c1ebad82ba1ac72bd765fc6b85f57948a9
alainabuzas <alaina.buzas@gmail.com>
2017-04-24 18:39:46 -0700

    trying to include author and comment

---
+++ dbTest.js
@@ -0,0 +1,16 @@
+var db = require('./models');
+
+// db.comment.create({
+//     name: 'Paul Allen',
+//     content: 'This is really neat! Thanks for posting.',
+//     postId: 1
+// }).then(function(comment) {
+//     console.log(comment.get());
+// });
+
+// db.post.find({
+            //     where: { id: 1 },
+            //     include: [db.comment]
+            // }).then(function(post) {
+            //     console.log(post.comments);
+            // });
