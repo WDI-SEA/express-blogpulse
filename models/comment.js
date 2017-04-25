99607f2e440c7d19c95a1b5663c89c6bb67738a2
jblneely <bowman.neely@gmail.com>
2017-04-24 18:30:59 -0700

    associations added

---
+++ models/comment.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+    var comment = sequelize.define('comment', {
+        name: DataTypes.STRING,
+        content: DataTypes.TEXT,
+        post: DataTypes.INTEGER
+    }, {
+        classMethods: {
+            associate: function(models) {
+                models.comment.belongsTo(models.author);
+            }
+        }
+    });
+    return comment;
+};
