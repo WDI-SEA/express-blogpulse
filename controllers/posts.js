acc4a93bae4a4a8b2503359a7203edd4e232b7e5
erikr93 <erikr93@gmail.com>
2017-09-26 10:32:30 -0700

    got the comments to post but without content

--- controllers/posts.js
+++ controllers/posts.js
@@ -8,32 +8,34 @@ router.post('/', function(req, res) {
     title: req.body.title,
     content: req.body.content,
     authorId: req.body.authorId
-  })
-  .then(function(post) {
+  }).then(function(post) {
     res.redirect('/');
-  })
-  .catch(function(error) {
+  }).catch(function(error) {
     res.status(400).render('main/404');
   });
 });
 
-router.post('/:id/comments', function(req, res) {
-   db.comment.create({
-     name: req.body.name,
-     content: req.body.content,
-     postId: req.params.id
-   }).then(function(comment){
-     res.redirect('/posts/' + req.params.id);
-   });
+router.post('/:id/comments', function(req, res){
+  db.post.findOne({
+    where: {id: req.params.id}
+  }).then(function(post){
+    post.createComment({
+      name: req.body.name,
+      content: req.body.content
+  }).then(function(){
+    console.log('comment created');
+    res.redirect('/posts/' + req.params.id);
+    })
+  }).catch(function(error){
+    res.status(400).render('main/404');
+  });
 });
 
 // GET /posts/new - display form for creating new posts
 router.get('/new', function(req, res) {
-  db.author.findAll()
-  .then(function(authors) {
+  db.author.findAll().then(function(authors) {
     res.render('posts/new', { authors: authors });
-  })
-  .catch(function(error) {
+  }).catch(function(error) {
     res.status(400).render('main/404');
   });
 });
@@ -43,12 +45,10 @@ router.get('/:id', function(req, res) {
   db.post.find({
     where: { id: req.params.id },
     include: [db.author, db.comment]
-  })
-  .then(function(post) {
+  }).then(function(post) {
     if (!post) throw Error();
     res.render('posts/show', { post: post });
-  })
-  .catch(function(error) {
+  }).catch(function(error) {
     res.status(400).render('main/404');
   });
 });
