b6306ec7a67c3a1d05b6010e7454f43c4ecdddde
Steven Peters <steve@Jennas-MacBook-Air.local>
2017-09-26 12:42:00 -0700

    CodeAlong updates

--- controllers/posts.js
+++ controllers/posts.js
@@ -10,6 +10,13 @@ router.post('/', function(req, res) {
     authorId: req.body.authorId
   })
   .then(function(post) {
+    db.tag.findOrCreate({
+      where: {name: req.body.tag}
+    }).spread(function(tag, created) {
+      post.addTag(tag).then(function() {
+        console.log("yippee");
+      })
+    })
     res.redirect('/');
   })
   .catch(function(error) {
@@ -32,7 +39,7 @@ router.get('/new', function(req, res) {
 router.get('/:id', function(req, res) {
   db.post.find({
     where: { id: req.params.id },
-    include: [db.author, db.comment]
+    include: [db.author, db.comment, db.tag]
   })
   .then(function(post) {
     if (!post) throw Error();
@@ -66,5 +73,17 @@ router.post('/:id/comments', function(req, res) {
   });
 });
 
+// GET for /posts/tag - returns all posts with a given tag
+router.get('/tags/:tag', function(req, res) {
+  console.log("Im in the tag get");
+  db.tag.findOne({
+    where: {name: req.params.tag}
+  }).then(function(tag) {
+    tag.getPosts().then(function(posts) {
+      console.log("These posts are tagged with " + tag.name + ":");
+      res.render('posts/index', {posts: posts});
+    })
+  })
+})
 
 module.exports = router;
