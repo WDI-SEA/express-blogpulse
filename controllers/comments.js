4a4be732ba21a78be1e5ec126a92c294b1c07fbe
hasstMac <hasstmac@hasstMacs-MacBook-Air.local>
2018-02-20 23:09:56 -0800

    homework finished

---
+++ controllers/comments.js
@@ -0,0 +1,51 @@
+// COMMENTS controller
+
+var express = require('express');
+var db = require('../models');
+var router = express.Router();
+
+// POST /comments - display all authors
+router.post('/', function(req, res) {
+  // console.log('in the /comments path...');
+  db.comment.create({
+    name:req.body.name,
+    content:req.body.content,
+    postId:req.body.postId
+  })
+  .then(function(comment){
+    res.redirect('posts/' + comment.postId);
+  })
+  .catch(function(error){
+    res.status(400).render('main/404');
+  });
+});
+
+// DELETE
+router.delete('/:id', function(req,res){
+  console.log('in delete/id path');
+    var deleteComment = {
+      id: req.params.id
+    }
+    db.comment.destroy({
+      where: {id:deleteComment.id}
+    }).then(function(comment){
+      console.log("successfully deleted..." + deleteComment.id)
+    });
+});
+
+// PUT
+router.put('/update', function(req,res){
+  db.comment.findById(req.body.id)
+  .then(function(comment){
+    db.comment.update({
+      content:req.body.content
+    },{
+      where: {id:req.params.id}
+    }).then(function(comment){
+      res.send('successfully updated');
+    });
+  });
+});
+
+
+module.exports = router;
--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,37 @@
 $(document).ready(function() {
 
+  // DELETE
+  $('.delete-item').on('click', function(e){
+  e.preventDefault();
+  var commentObj = $(this);
+  var commentUrl = commentObj.attr('href');
+  console.log("hit the delete link");
+  $.ajax({
+    method: 'DELETE',
+    url: commentUrl
+  }).done(function(comment){
+    window.location = "/posts";
+  });
+});
+
+  // PUT
+  $('.put-form').on('submit', function(e){
+  e.preventDefault();
+  var commentObj = $(this);
+  var commentUrl = commentObj.attr('action');
+  var commentData = commentObj.serialize();
+  $.ajax({
+    method: 'PUT',
+    url: commentUrl,
+    data: commentData
+  }).done(function(comment){
+    window.location = '/posts';
+  });
+});
+
+
+  $('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+  })
+
 });
