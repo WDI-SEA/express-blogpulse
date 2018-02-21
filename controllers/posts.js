d1552890c2e5be2fc5041fe83499898d82f717e1
Emily Garrett <emilygarrett@Emilys-MacBook-Pro.local>
2018-02-20 20:51:07 -0800

    deliverable done w styling

--- controllers/posts.js
+++ controllers/posts.js
@@ -32,7 +32,7 @@ router.get('/new', function(req, res) {
 router.get('/:id', function(req, res) {
   db.post.find({
     where: { id: req.params.id },
-    include: [db.author]
+    include: [db.author, db.comment]
   })
   .then(function(post) {
     if (!post) throw Error();
@@ -42,5 +42,18 @@ router.get('/:id', function(req, res) {
     res.status(400).render('main/404');
   });
 });
+
+router.post('/:id', function(req,res) {
+  db.post.find({
+    where: {id: req.body.postId }
+  }).then(function(post) {
+    post.createComment({
+      name: req.body.name,
+      content:req.body.content
+    }).then(function(comment) {
+      res.redirect('/posts/' + req.params.id);
+    })
+  })
+})
 
 module.exports = router;
--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
+	$('#delete-comment').on('click', function(e) {
+	e.preventDefault();
+	var thisComment = $(this);
+	var destination = thisComment.attr('href');
+	$.ajax({
+		method: 'DELETE', 
+		url: destination
+	}).done(function(data) {
+		console.log(data);
+	});
+});
 
+
+$(".button-collapse").sideNav();
 });
