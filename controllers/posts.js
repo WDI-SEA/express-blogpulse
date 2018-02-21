7ed3e4831aaf1ee23829f17f54f67deabe9500a3
hannan-emily <emilyhannan@gmail.com>
2018-02-20 21:12:47 -0800

    finished all deliverable requirements - styling with materialize is ongoing

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,6 +2,8 @@ var express = require('express');
 var db = require('../models');
 var router = express.Router();
 
+router.use(express.static(__dirname + '/public/'));
+
 // POST /posts - create a new post
 router.post('/', function(req, res) {
   db.post.create({
@@ -17,6 +19,22 @@ router.post('/', function(req, res) {
   });
 });
 
+//post comments to THIS post id
+router.post('/:id', function(req,res) { //creating a new comment
+  db.post.find({
+    where: { id: req.body.postId } //find the associated post by id number
+  }).then(function(post) { //using that author, add THIS post to THIS author in the author database
+    //format =  table.createModel()
+      post.createComment({
+      name: req.body.name, //loading data from our form input fields
+      content: req.body.content //loading data from our form input fields
+    }).then(function(comment) { //load child
+      res.redirect('/posts/' + req.params.id); //send comment
+    });
+  });
+});
+
+
 // GET /posts/new - display form for creating new posts
 router.get('/new', function(req, res) {
   db.author.findAll()
@@ -28,11 +46,11 @@ router.get('/new', function(req, res) {
   });
 });
 
-// GET /posts/:id - display a specific post and its author
+// GET /posts/:id - display a specific post and its author and its comments??
 router.get('/:id', function(req, res) {
   db.post.find({
     where: { id: req.params.id },
-    include: [db.author]
+    include: [db.author,db.comment]
   })
   .then(function(post) {
     if (!post) throw Error();
@@ -40,6 +58,20 @@ router.get('/:id', function(req, res) {
   })
   .catch(function(error) {
     res.status(400).render('main/404');
+  });
+});
+
+//DELETE comment
+router.delete('/:id', function(req, res) {
+  db.post.find({
+    where: { id: req.body.postId } //find the associated post by id number
+  }).then(function(post) { //using that author, add THIS post to THIS author in the author database
+    //format =  table.createModel()
+      post.comment.destroy({
+        where: {name: req.params.name}
+    }).then(function(comment) { //load child
+      res.redirect('/posts/' + req.params.id); //send comment
+    });
   });
 });
 
--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
 
+  $('.delete-link').on('click', function(e) {
+    e.preventDefault();
+    var comment = $(this);
+    var commentUrl = comment.attr('href');
+    $.ajax({
+      method: 'DELETE',
+      url: commentUrl
+    }).done(function(data) {
+      // get data returned from the DELETE route
+      console.log(data);
+
+      // do stuff when the DELETE action is complete
+      comment.remove();
+
+      // or, you can redirect to another page
+      window.location = '/posts/<%= post.id %>';
+    });
+  });
+
 });
