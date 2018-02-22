f53672806b34cf8e8da8642bded1d4fa6981a8b3
Zakir Butte <zakir.butte@gmailcom>
2018-02-21 16:54:39 -0800

    testing

--- dbTest.js
+++ dbTest.js
@@ -8,3 +8,8 @@ db.post.find({
   // by using eager loading, the post model should have a comments key
   console.log(post.comments);
 });
+
+// where: {id: req.params.id},
+// include: [{model: db.post,
+//   include: [{model: db.post.author, where: {id: db.post.authorId}}]
+// }]
