a56214abea5de54968a3610abd67104d5105b7f5
Scott Ammon <scott.o.ammon@gmail.com>
2018-06-19 09:37:44 -0700

    fixed delete route and format of comments post route

--- controllers/comments.js
+++ controllers/comments.js
@@ -2,21 +2,30 @@ var express = require('express');
 var db = require('../models');
 var router = express.Router();
 
-// POST /posts/comments - add a new comment to a post
-router.post('/:id', function(req, res) {
+// POST /comments - add a new comment to a post
+router.post('/', function(req, res) {
    // find the current post
-  db.post.findById(req.params.id).then(function(post) {
-    // create a comment specifically for that author
+  db.post.findById(req.body.postId).then(function(post) {
+    // create a comment specifically for that post
     post.createComment({
       name: req.body.name,
-      content: req.body.content,
-      postId: post.id
+      content: req.body.content
+      // don't need the postId column... it will add automatically
     }).then(function(post) {
       res.redirect('/posts/' + post.postId);
     }).catch(function(error) {
       res.status(400).render('main/404');
     });
   });
+});
+
+router.delete('/:id', function(req, res) {
+	console.log('comment to delete: ', req.params.id);
+	db.comment.destroy({
+		where: {id: req.params.id}
+	}).then(function(data) {
+    res.sendStatus(200);
+	});
 });
 
 module.exports = router;
--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
 
+  // DELETE a specific comment from the database
+  $(".delete").on('click', function(e) {
+    e.preventDefault();
+    var url = $(this).attr('href');
+    var currentPost = $(this).attr('id');
+    $.ajax({
+      method: 'DELETE',
+      url: url
+    }).done(function(data) {
+      window.location = '/posts/' + currentPost;
+    })
+  });
 });
