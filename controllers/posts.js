d22b6e87ad8af8782edf33bb20f63c53228e8b2c
Genevieve Schultz <genschultz@gmail.com>
2018-06-18 23:30:40 -0700

    updated routes and added comment routes

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,89 +2,70 @@ var express = require('express');
 var db = require('../models');
 var router = express.Router();
 
+// GET / - display all posts and their authors
+router.get('/', function (req, res) {
+  db.post.findAll({
+    include: [db.author]
+  }).then(function (posts) {
+    res.render('posts/index', { posts: posts });
+  }).catch(function (error) {
+    res.status(400).render('main/404');
+  });
+});
+
 // POST /posts - create a new post
-router.post('/', function(req, res) {
+router.post('/', function (req, res) {
   db.post.create({
     title: req.body.title,
     content: req.body.content,
     authorId: req.body.authorId
-  })
-  .then(function(post) {
-    db.tag.findOrCreate({
-      where: {name: req.body.tag}
-    }).spread(function(tag, created) {
-      post.addTag(tag).then(function() {
-        console.log("yippee");
-      })
-    })
+  }).then(function (post) {
     res.redirect('/');
-  })
-  .catch(function(error) {
+  }).catch(function (error) {
+    console.log(error)
     res.status(400).render('main/404');
   });
 });
 
 // GET /posts/new - display form for creating new posts
-router.get('/new', function(req, res) {
-  db.author.findAll()
-  .then(function(authors) {
+router.get('/new', function (req, res) {
+  db.author.findAll().then(function (authors) {
     res.render('posts/new', { authors: authors });
-  })
-  .catch(function(error) {
+  }).catch(function (error) {
     res.status(400).render('main/404');
   });
 });
 
 // GET /posts/:id - display a specific post and its author
-router.get('/:id', function(req, res) {
+router.get('/:id', function (req, res) {
   db.post.find({
     where: { id: req.params.id },
-    include: [db.author, db.comment, db.tag]
-  })
-  .then(function(post) {
+    include: [db.author, db.comment]
+  }).then(function (post) {
     if (!post) throw Error();
     res.render('posts/show', { post: post });
-  })
-  .catch(function(error) {
+  }).catch(function (error) {
     console.log(error);
     res.status(400).render('main/404');
   });
 });
 
-
-// GET /posts/:id - display a specific post and its author
-router.post('/:id/comments', function(req, res) {
-  console.log("POST ROUTE TO /posts/ID/comments");
+// POST /posts/:id - submit a comment on a specific post
+router.post('/:id/comments', function (req, res) {
+  // Display form for creating a comment
   db.post.findOne({
     where: { id: req.params.id }
-  })
-  .then(function(post) {
-    console.log("NOW IN THEN - FOUND POST");
+  }).then(function (post) {
     post.createComment({
       name: req.body.name,
       content: req.body.content
-    }).then(function() {
-      console.log("NOW IN THEN - CREATED COMMENT");
+    }).then(function () {
       res.redirect('/posts/' + req.params.id);
-    })
-  })
-  .catch(function(error) {
-    console.log("CATCH ERROR");
+    });
+  }).catch(function (error) {
+    console.log(error);
     res.status(400).render('main/404');
   });
 });
-
-// GET for /posts/tag - returns all posts with a given tag
-router.get('/tags/:tag', function(req, res) {
-  console.log("Im in the tag get");
-  db.tag.findOne({
-    where: {name: req.params.tag}
-  }).then(function(tag) {
-    tag.getPosts().then(function(posts) {
-      console.log("These posts are tagged with " + tag.name + ":");
-      res.render('posts/index', {posts: posts});
-    })
-  })
-})
 
 module.exports = router;
