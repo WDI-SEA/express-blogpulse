f630f0fd6bec174e9f69e0fba1520b81bc456870
Matt Carver <mattcarver.wd@gmail.com>
2018-06-19 14:40:04 -0700

    ready for intermediary table

--- controllers/comments.js
+++ controllers/comments.js
@@ -2,34 +2,18 @@ var express = require('express');
 var db = require('../models');
 var router = express.Router();
 
-
+//POST /comments - adds a new comment.
 router.post('/', function(req, res) {
-  db.comment.create({
-    name: req.body.name,
-    content: req.body.content
-    postId: req.params.id
-  }).then(function(comment) {
-      res.redirect('/');
-    })
-    .catch(function(error) {
-      res.status(400).render('main/404');
+  db.post.findById(req.body.postId).then(function(post){
+    post.createComment({
+      name: req.body.name,
+      content: req.body.content
+    }).then(function(comment) {
+      console.log(comment);
+      res.redirect("/posts/" + req.body.postId);
     });
   });
-
-router.get('/:id', function(req, res) {
-  db.comment.find({
-    where: { id: req.params.id },
-    include: [db.comment]
-  }).then(function(post) {
-    if (!post) throw Error();
-      res.render('comments/show', { comment: comment });
-    })
-    .catch(function(error) {
-      res.status(400).render('main/404');
-    });
-  });
+});
 
 
-//   // by using eager loading, the post model should have a comments key
-//   console.log(post.comments);
-// });
+module.exports = router;
