f7dfbfff5599d8fe58922e80924e7253c592993b
Eva Barrett <Evabarrett23@Gmail.com>
2018-06-19 10:01:15 -0700

    fixed comments page

---
+++ controllers/comments.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var db = require('../models');
+var router = express.Router();
+
+// POST /comments - adds a new comment
+router.post('/', function(req, res) {
+  db.post.findById(req.body.postId).then(function(post) {
+    post.createComment({
+      name: req.body.name,
+      content: req.body.content
+    }).then(function(comment) {
+      console.log(comment);
+      res.redirect('/posts/' + req.body.postId);
+    });
+  });
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -35,6 +35,8 @@ app.get('/', function(req, res) {
 // bring in authors and posts controllers
 app.use('/authors', require('./controllers/authors'));
 app.use('/posts', require('./controllers/posts'));
+// if you added a comments under controllers
+app.use('/comments', require('./controllers/comments'));
 
 var server = app.listen(process.env.PORT || 3000, function() {
   rowdy.print();
