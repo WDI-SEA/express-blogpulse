f36466b9f96a12253844c2955976cbefe0ab1983
ericaschenker <ericaschenker@gmail.com>
2018-06-19 09:46:16 -0700

    Got him working w/ help from Steve, woooo

--- controllers/comments.js
+++ controllers/comments.js
@@ -3,17 +3,15 @@ var db = require('../models');
 var router = express.Router();
 
 // POST comment on specific article
-router.post('/:id', function(req,res) {
-  db.post.find({
-    where: {id: req.body.postId }
-  }).then(function(post) {
-    post.createComment({
-      name: req.body.name,
-      content:req.body.content
-    }).then(function(comment) {
-      res.redirect('/posts/' + req.params.id);
-    })
-  })
-})
+router.post('/', function(req, res) {
+	db.post.findById(req.body.postId).then(function(post) {
+		post.createComment({
+			name: req.body.name,
+			content: req.body.content
+		}).then(function(comment) {
+			res.redirect('/posts/' + req.body.postId);
+		})
+	});
+});
 
 module.exports = router;
