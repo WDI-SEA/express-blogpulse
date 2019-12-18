21fd49267db388c4c16c04f836a56be7ad72bea8
shannonzylstra <shannonzylstra@gmail.com>
2019-12-18 07:27:39 -0800

    Comments display on /articles/:id page

--- controllers/articles.js
+++ controllers/articles.js
@@ -28,15 +28,16 @@ router.get('/new', function(req, res) {
   })
 })
 
-// GET /articles/:id - display a specific post and its author
+// GET /articles/:id - display a specific post and its author and any comment(s)
 router.get('/:id', function(req, res) {
   db.article.findOne({
     where: { id: req.params.id },
-    include: [db.author]
+    include: [db.author, db.comment]
   })
   .then(function(article) {
     if (!article) throw Error()
     console.log(article.author)
+    console.log(article.comments)
     res.render('articles/show', { article: article })
   })
   .catch(function(error) {
