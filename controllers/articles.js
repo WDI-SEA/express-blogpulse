1205de2b91df7cd5641da0a046bfd17bcb934b8d
snipspin <photonenimpuls@gmail.com>
2020-02-19 22:26:29 -0800

    finished part two and the first bonus

--- controllers/articles.js
+++ controllers/articles.js
@@ -16,7 +16,22 @@ router.post('/', function(req, res) {
     res.status(400).render('main/404')
   })
 })
-
+router.get('/edit/:id', function(req, res) {
+// res.send(req.params);
+  db.article.findOne({
+    where: { id: req.params.id },
+    include: [db.author]
+  })
+  .then(function(article) {
+    if (!article) throw Error()
+    console.log(article.comments)
+    res.render('articles/edit', { article: article })
+  })
+  .catch(function(error) {
+    console.log(error)
+    res.status(400).render('main/404')
+  })
+})
 // GET /articles/new - display form for creating new articles
 router.get('/new', function(req, res) {
   db.author.findAll()
@@ -32,17 +47,45 @@ router.get('/new', function(req, res) {
 router.get('/:id', function(req, res) {
   db.article.findOne({
     where: { id: req.params.id },
-    include: [db.author]
+    include: [db.author, db.comment]
   })
   .then(function(article) {
     if (!article) throw Error()
-    console.log(article.author)
-    res.render('articles/show', { article: article })
+    console.log(article.comments)
+    res.render('articles/show', { article: article, comments: article.comments })
   })
   .catch(function(error) {
     console.log(error)
     res.status(400).render('main/404')
   })
 })
+
+router.post('/edit', (req,res)=>{
+  db.article.update({
+    id: req.body.id,
+    title: req.body.title,
+    content: req.body.content
+  }, {
+    where: {
+      id: req.body.id
+    }
+  }).then(()=>{
+    res.redirect(`/articles/${req.body.id}`);
+  }).catch((error) => {
+    console.log(error);
+    res.status(400).render('main/404')
+  });
+  
+});
+
+router.post('/articles/:id/comments', (req,res) => {
+  db.comment.create({
+    name: req.body.author,
+    content: req.body.content,
+    articleId: req.params.id
+  }).then((response) => {
+    res.redirect(`/articles/${req.params.id}`);
+  })
+});
 
 module.exports = router
