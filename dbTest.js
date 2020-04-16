4b285be17a7a6f5f6a066ca0e2fe37daefac7e6a
gjones81 <jones.guy81@gmail.com>
2020-04-15 18:39:29 -0700

    Updating

---
+++ dbTest.js
@@ -0,0 +1,11 @@
+let express = require('express')
+let db = require('./models')
+
+db.comment.create({
+	name: 'Paul Allen',
+	content: 'This is really neat! Thanks for posting.',
+	articleId: 1
+})
+.then(function(comment) {
+	console.log(comment.get())
+})
