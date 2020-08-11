45aa3e76cc742036dffd6791bee46fcc5b27814d
General Assembly <generalassembly@Davids-MacBook-Pro.local>
2020-08-10 22:08:50 -0700

    [comments] Add a few more comments to different articles

--- dbTest.js
+++ dbTest.js
@@ -1,18 +1,18 @@
 const db = require('./models')
 
-// db.comment.create({
-//   commentator: 'Paul Allen',
-//   commentContent: 'This is really neat! Thanks for posting.',
-//   articleId: 1
-// })
-// .then(function(comment) {
-//   console.log(comment.get())
-// })
+db.comment.create({
+  commentator: 'David Cabassa',
+  commentContent: 'SO GLAD for the new bike lanes! Lets turn ALL the streets into bike lanes!',
+  articleId: 1
+})
+.then(function(comment) {
+  console.log(comment.get())
+})
 
-db.article.findOne({
-    where: { id: 1 },
-    include: [db.comment]
-  }).then(function(article) {
-    // by using eager loading, the article model should have a comments key
-    console.log(article.comments)
-  })
+// db.article.findOne({
+//     where: { id: 1 },
+//     include: [db.comment]
+//   }).then(function(article) {
+//     // by using eager loading, the article model should have a comments key
+//     console.log(article.comments)
+//   })
