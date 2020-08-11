7e4b399d6913ba2aade680553db73fe2a709fbef
blangwell <b.langwell@outlook.com>
2020-08-10 20:09:04 -0700

    [models] Recreate comment model, migrate to db, test in dbTest

--- dbTest.js
+++ dbTest.js
@@ -15,12 +15,26 @@ const db = require('./models')
 //     console.log(article.comments)
 // })
 
-db.article.findOne().then(article => {
+// db.article.findOne().then(article => {
+//     article.createComment({
+//         author: 'Some dog',
+//         content: 'bark bark bark',
+//         articleId: 2
+//     }).then(comment => {
+//         console.log(comment.content)
+//     })
+// })
+
+db.article.findOne({
+    where: {id: 1}
+})
+.then(article=>{
     article.createComment({
-        author: 'Some dog',
-        content: 'bark bark bark',
-        articleId: 2
-    }).then(comment => {
-        console.log(comment.content)
+        author: 'aiden bryce taylor',
+        content: 'today is my dads birday!:)',
+        articleId: article.id
+    })
+    .then(comment=>{
+        console.log(`created the comment : ${comment.content}`)
     })
 })
