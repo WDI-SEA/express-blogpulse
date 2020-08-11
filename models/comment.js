874b06f924bdc3cd17be7a0afd87b0c520f3293b
blangwell <b.langwell@outlook.com>
2020-08-10 19:17:59 -0700

    [models] Associate comment and article models

--- models/comment.js
+++ models/comment.js
@@ -10,7 +10,8 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+        // define association here
+      models.comment.belongsTo(models.article)
     }
   };
   comment.init({
