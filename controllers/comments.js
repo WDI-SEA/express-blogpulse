768c7a539552e15d5398f06d9bd487d9124ed53c
General Assembly <generalassembly@Davids-MacBook-Pro.local>
2020-08-13 00:59:26 -0700

    [comment] FINALLY add correct form and controller route

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,5 +1,7 @@
 let express = require('express')
 let db = require('../models')
+const comment = require('../models/comment')
+const article = require('../models/article')
 let router = express.Router()
 
 // POST /comments
@@ -7,10 +9,10 @@ router.post('/', (req, res) => {
     db.comment.create({
       commentator: req.body.commentator,
       commentContent: req.body.commentContent,
-      articleId: req.params.id
+      articleId: req.body.articleId
     })
     .then((post) => {
-      res.redirect('/')
+      res.redirect(`/articles/${req.body.articleId}`)
     })
     .catch((error) => {
       res.status(400).render('main/404')
