6483452b7a81f0e1af2d76724080be52578c2593
Chamon562 <Chamon562@gmail.com>
2020-08-12 22:27:18 -0700

    [view] 5 hours still cant get my comment database to show up.

--- controllers/articles.js
+++ controllers/articles.js
@@ -29,6 +29,7 @@ router.get('/new', (req, res) => {
 })
 
 // GET /articles/:id - display a specific post and its author
+// ('/:id') already in articles and its parameter is :id i find it in where {id:req.params.id}
 router.get('/:id', (req, res) => {
   db.article.findOne({
     where: { id: req.params.id },
@@ -45,4 +46,82 @@ router.get('/:id', (req, res) => {
   })
 })
 
+router.get('/:id', (req, res)=>{
+  db.comment.findOne({
+    where: {id: req.params.id}
+    // include: [db.article]
+  })
+ .then(comment =>{
+   console.log(comment.project)
+   res.redirect('articles')
+ })
+ .catch(err =>{
+   console.log('ERRrrrrrrRRRRRRRRRRORRRRRRRRR', err)
+ })
+})
+
+
+
+
+
+
+
+// router.get('/', (req, res)=>{
+//   db.category.findAll()
+//   .then(categories =>{
+//       res.render('categories/index', {categories:categories})
+//   }).catch(err =>{console.log(err)})
+// })
+
+
+
+// router.get('/:id', (req, res) =>{
+  //purpose choose a category and find all the projects that assocaied withthat cataogry
+  //find with req.params.id from the url
+  // db.category.findOne({
+  //     where: {id: req.params.id},
+  //     include: [db.project]
+  // })
+//   .then(category =>{
+//       //rpove page with category object
+//       console.log(category.projects)
+//       res.render('categories/show', {category})
+//   }).catch(err =>{console.log('error', err)})
+// })
+
+// router.post('/', (req, res) =>{
+
+// })
+
+
+
+
+
+// db.artist.findOrCreate({
+//   where: {name: '2pac'},
+//   defaults: {city: 'West Coast California', description: 'Poet' }
+// })
+// .then(([artist, created])=>{
+//   console.log(created)
+//   db.album.findOrCreate({
+//       where: {title: 'All Eyez On Me'},
+//       defaults: {year: '1996', genre: 'hip-hop'}
+//   })
+//   .then(([album, created])=>{
+//       console.log(created)
+//       artist.addAlbum(album)
+//       .then(relationshipInfo =>{
+//           console.log(relationshipInfo)
+//       })
+//       .catch(err =>{
+//           console.log('Error', err)
+//       })
+//   })
+//   .catch(err =>{
+//       console.log('Error', err)
+//   })
+// })
+// .catch(err =>{
+//   console.log('Error', err)
+// })
 module.exports = router
--- dbTest.js
+++ dbTest.js
@@ -1,7 +1,5 @@
-
-
+var db = require("./models");
 // var db = require('./models')
-
 // db.comment.create({
 //   name: 'Paul Allen',
 //   content: 'This is really neat! Thanks for posting.',
@@ -11,16 +9,6 @@
 //   console.log(comment.get())
 // })
 
-
-var db = require('./models')
-
-db.article.findOne({
-    where: { id: 2 },
-    include: [db.comment]
-})
-.then((article)=>{
-    console.log(article.comments)
-})
 // db.article.findOne({
 //   where: { id: 1 },
 //   include: [db.comment]
@@ -29,6 +17,7 @@ db.article.findOne({
 //   console.log(article.comments)
 // })
 
+// what I created a comment
 // db.comment.create({
 //     name: 'Channee Math',
 //     content: 'I just want everyone to know, that if I make it, we all gon make it. Real shit',
@@ -37,4 +26,77 @@ db.article.findOne({
 // .then((comment)=>{
 //     console.log(comment.get())
 // })
+//what i also created is to find my article by id
+// db.article.findOne({
+//     where: { id: 2 },
+//     include: [db.comment]
+// })
+// .then((article)=>{
+//     console.log(article.comments)
+// })
 
+// db.article.create({
+//     title: 'Long Beach Press Telegram: Eastside Long Beach Native tech sensation comes back to help uplift his community',
+//     content: 'Channee Math was the middle child out of 8 siblings, and was no stranger to poverty. His parents escaped the Khmai rouge by heading over to Thailand first, where his 2 oldest brothers even had to eat snake for food to survives as babies. They somehow made it to th United States only to be reintroduced to another world of savage racism and gang culter. This young man has truely seen it all and is now trying to help uplift his own community by creating a non profit tech school and also shops to help those without much. ',
+//     authorId: 6
+// })
+// .then((article)=>{
+//     console.log(article.get())
+// })
+
+//created my own to find my article by id that I wrote
+// db.article.findOne({
+//     where: {id: 8},
+
+// })
+// .then((article)=>{
+//     console.log(article)
+// })
+// db.author.create({
+//     firstName: "Channee",
+//     lastName: "Math",
+//     bio:
+//       "Software Engineer. Father. Husband. Rapper/Producer/Song. Writer Poet.",
+//   })
+//   .then((authors) => {
+//     console.log(authors.get());
+//   });
+
+// db.author.findOne({
+//     where:{firstName: 'Channee'}
+// })
+// .then((author) =>{
+//     console.log(author)
+// })
+
+// db.comment.create({
+//     name: 'CM Live',
+//     content: 'Write lyrics like a dead poet, dont like socializin but I hope my point is clear so I ant gotta show it, never a failure to fear, but i hope my dreams appear so i can finally make this money and grow it. A future im fine with not knowin, blue city sky blue palm trees cruisin down ocean, that off white 2 door 2000 and 5 integra stay coastin, made it past 16 feelin like I was chosin, no matter how many times I fall, they can never stop a real one from gettin where he goin, Real shit, I hope you forver blessed, and I wish you nothin but success. its Live',
+//     articleId: 3
+// })
+// .then((comment)=>{
+//     console.log(comment.get())
+// })
+
+// db.article.findOne()
+// .then(foundarticle=>{
+//     foundarticle.createComment({
+//         //and add a comment
+//         name: 'Hater guy',
+//         content: 'your article really freaken sucks'
+//     })
+//     .then(createComment =>{
+//         console.log(createComment.get())
+//     })
+// })
+
+db.article.findOne({
+    where: {id: 1},
+    include: [db.comment]
+})
+.then(article =>{
+    //only apply get to one row of data
+   article.comments.forEach(comment =>{
+       console.log(comment.get())
+   })
+})
