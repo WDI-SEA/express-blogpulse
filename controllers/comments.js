fb3454e0a541d145449d9e3fd200fd38c02768dd
General Assembly <generalassembly@Davids-MacBook-Pro.local>
2020-08-12 23:56:33 -0700

    [show.ejs] Fix add comment form

---
+++ controllers/comments.js
@@ -0,0 +1,20 @@
+let express = require('express')
+let db = require('../models')
+let router = express.Router()
+
+// POST /comments
+router.post('/', (req, res) => {
+    db.comment.create({
+      commentator: req.body.commentator,
+      commentContent: req.body.commentContent,
+      articleId: req.body.articleId
+    })
+    .then((post) => {
+      res.redirect('/')
+    })
+    .catch((error) => {
+      res.status(400).render('main/404')
+    })
+  })
+
+module.exports = router
