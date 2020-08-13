2424e209bac6b493a41452b98305ac54319e426b
General Assembly <generalassembly@Davids-MacBook-Pro.local>
2020-08-12 23:13:41 -0700

    [show.ejs] Add article comments to article view

--- dbTest.js
+++ dbTest.js
@@ -1,13 +1,13 @@
 const db = require('./models')
 
-db.comment.create({
-  commentator: 'David Cabassa',
-  commentContent: 'SO GLAD for the new bike lanes! Lets turn ALL the streets into bike lanes!',
-  articleId: 1
-})
-.then(function(comment) {
-  console.log(comment.get())
-})
+// db.comment.create({
+//   commentator: 'David Cabassa',
+//   commentContent: 'SO GLAD for the new bike lanes! Lets turn ALL the streets into bike lanes!',
+//   articleId: 1
+// })
+// .then(function(comment) {
+//   console.log(comment.get())
+// })
 
 // db.article.findOne({
 //     where: { id: 1 },
@@ -16,3 +16,13 @@ db.comment.create({
 //     // by using eager loading, the article model should have a comments key
 //     console.log(article.comments)
 //   })
+
+db.article.findOne({
+    where: {id: 1},
+    include: [db.comment]
+})
+.then(article=>{
+    article.comments.forEach(comment=>{
+        console.log(comment.get())
+    })
+})
