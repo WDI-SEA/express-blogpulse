f2f3075d0ca9b73398f031633ffaf48d3c4b2c76
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-06-03 18:01:34 -0500

    Wrote rough draft comments get/post functions. Require testing.

--- controllers/comments.js
+++ controllers/comments.js
@@ -3,8 +3,23 @@ let db = require('../models')
 let router = express.Router()
 
 // GET /comments/new - display form for creating new comments
+router.get('/new', (req, res) => {
+    res.render('comments/new.ejs')
+  })
 
-// POST /articles - create a new comment
+// POST /comments - create a new comment
+router.post('/', (req, res) => {
+    db.comment.create({
+      name: req.body.name,
+      content: req.body.content,
+    })
+    .then((post) => {
+      res.redirect('/')
+    })
+    .catch((error) => {
+      res.status(400).render('main/404')
+    })
+  })
 
 
 module.exports = router
