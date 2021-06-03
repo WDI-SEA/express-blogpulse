5b116dd115b5ad4a238fe8221133e0b49d97bad0
miknayr <teamry4n@gmail.com>
2021-06-03 15:28:50 -0700

    need to migrate db

--- models/comment.js
+++ models/comment.js
@@ -1,15 +1,25 @@
-'use strict'
+'use strict';
+const {
+  Model
+} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  const comment = sequelize.define('comment', {
+  class comment extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  comment.init({
     title: DataTypes.STRING,
     content: DataTypes.TEXT,
     articleId: DataTypes.INTEGER
-  }, {})
-
-  comment.associate = function(models) {
-    // associations can be defined here
-    models.comment.belongsTo(models.article)
-  }
-  
-  return comment
-}
+  }, {
+    sequelize,
+    modelName: 'comment',
+  });
+  return comment;
+};
