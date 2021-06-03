51d2b74774668aad3c84e70def8803037a31535f
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-06-03 17:57:01 -0500

    Created views/comments folder, created comments new.ejs file.

--- controllers/comments.js
+++ controllers/comments.js
@@ -2,5 +2,9 @@ let express = require('express')
 let db = require('../models')
 let router = express.Router()
 
+// GET /comments/new - display form for creating new comments
+
+// POST /articles - create a new comment
+
 
 module.exports = router
