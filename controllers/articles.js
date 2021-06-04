7c6541efddb5923497b7ab7783916cd399113f0c
Ellie-Lissner <eswalker0723@gmail.com>
2021-06-03 22:57:20 -0500

    added comment feature

--- controllers/articles.js
+++ controllers/articles.js
@@ -30,14 +30,33 @@ router.get('/new', (req, res) => {
 
 // GET /articles/:id - display a specific post and its author
 router.get('/:id', (req, res) => {
+  console.log('here')
   db.article.findOne({
     where: { id: req.params.id },
-    include: [db.author]
+    include: [db.author, db.comment]
   })
   .then((article) => {
     if (!article) throw Error()
+    console.log(article.comments)
     console.log(article.author)
     res.render('articles/show', { article: article })
+  })
+  .catch((error) => {
+    console.log(error)
+    res.status(400).render('main/404')
+  })
+})
+
+router.post('/:id/comments', (req, res) => {
+  db.comment.create({
+    name: req.body.name,
+    thoughts: req.body.thoughts,
+    articleId: req.params.id
+  })
+  .then(comment => {
+    console.log(comment)
+    res.redirect(`/articles/${req.params.id}`)
+
   })
   .catch((error) => {
     console.log(error)
