fdfb1c7a8f142ccdb2c6e8b55b5a155d2de26211
miknayr <teamry4n@gmail.com>
2021-06-04 11:59:19 -0700

    added some styling

--- dbTest.js
+++ dbTest.js
@@ -1,19 +1,19 @@
 const db = require('./models')
 
-// db.comment.create({
-//   commentName: 'Paul Allen',
-//   content: 'This is really neat! Thanks for posting.',
-//   articleId: 1
-// })
-// .then(comment => {
-//   console.log(comment.get())
-// })
-
-
-db.article.findOne({
-  where: { id: 1 },
-  include: [db.comment]
-}).then(article => {
-  // by using eager loading, the article model should have a comments key
-  console.log(article.comments)
+db.comment.create({
+  commentName: 'Paul Allen',
+  content: 'This is really neat! Thanks for posting.',
+  articleId: 1
 })
+.then(comment => {
+  console.log(comment.get())
+})
+
+
+// db.article.findOne({
+//   where: { id: 1 },
+//   include: [db.comment]
+// }).then(article => {
+//   // by using eager loading, the article model should have a comments key
+//   console.log(article.comments)
+// })
