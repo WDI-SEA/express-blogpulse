ef29cbb4834caed08547c8b51600baa6e3cac346
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-06-04 11:21:25 -0500

    Deleted unnecessary views/comment files. Fixed homepage bug. Still need to return to article.

--- controllers/comments.js
+++ controllers/comments.js
@@ -2,11 +2,6 @@ let express = require('express')
 let db = require('../models')
 let router = express.Router()
 
-// GET /comments/new - display form for creating new comments
-// router.get('/new', (req, res) => {
-//     res.render('comments/new.ejs')
-//   })
-
 // POST /comments - create a new comment
 router.post('/', (req, res) => {
     db.comment.create({
@@ -15,7 +10,7 @@ router.post('/', (req, res) => {
       articleId: req.body.articleId
     })
     .then((post) => {
-      res.redirect('/authors')
+      res.redirect("/authors")
     })
     .catch((error) => {
       res.status(400).render('main/404')
