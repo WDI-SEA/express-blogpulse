74b15bf41ffb34cdc4b291fd68ef97686a2c0ff1
hamazumi <khamamoto12@gmail.com>
2021-06-03 17:04:15 -0700

    Add ability to view comment

--- dbTest.js
+++ dbTest.js
@@ -1,18 +1,18 @@
 const db = require('./models')
 
-// db.comment.create({
-//   name: 'Paul Allen',
-//   content: 'This is really neat! Thanks for posting.',
-//   articleId: 1
-// })
-// .then(comment => {
-//   console.log(comment.get())
-// })
+db.comment.create({
+  commentName: 'Paul Allen',
+  content: 'This is really neat! Thanks for posting.',
+  articleId: 1
+})
+.then(comment => {
+  console.log(comment.get())
+})
 
-db.article.findOne({
-    where: { id: 1 },
-    include: [db.comment]
-  }).then(article => {
-    // by using eager loading, the article model should have a comments key
-    console.log(article.comments)
-  })
+// db.article.findOne({
+//     where: { id: 1 },
+//     include: [db.comment]
+//   }).then(article => {
+//     // by using eager loading, the article model should have a comments key
+//     console.log(article.comments)
+//   })
