f7ab90d94f6fc4a428792f19a5592aaced1b9010
kubeshauseli17 <kubeshauseli17@gmail.com>
2022-09-09 09:33:52 -0700

    finally got the tables working

--- models/article.js
+++ models/article.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      models.article.hasMany(models.comment)
       models.article.belongsTo(models.author)
     }
   }
