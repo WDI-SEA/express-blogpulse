b949689dccb8da8b30fec20b8e7fa8bdae972fd9
average-alien <allentang113@gmail.com>
2022-09-08 21:17:54 -0700

    add new comments and shows them

--- db-test.js
+++ db-test.js
@@ -23,4 +23,26 @@ const createComment = async () => {
   }
 }
 
-createComment()
+// createComment()
+
+db.article.findOne({
+  where: { id: 1 },
+  include: [db.comment]
+}).then(article => {
+  // by using eager loading, the article model should have a comments key
+  console.log(article.comments)
+})
+
+const readArticles = async () => {
+  try {
+    const article = await db.article.findOne({
+      where: { id: 2},
+      include: [db.author]
+    })
+    console.log(article)
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+readArticles()
