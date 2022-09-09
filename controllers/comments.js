0558111e92c82e3ca3dcd8b71218680c3cf447ee
joshdewhurst <joshuardewhurst@gmail.com>
2022-09-09 08:41:28 -0700

    joshs deliverable

---
+++ controllers/comments.js
@@ -0,0 +1,22 @@
+let express = require('express')
+let db = require('../models')
+const comment = require('../models/comment')
+let router = express.Router()
+
+router.get("/", async (req,res)=>{
+    const comment = await db.comment.findAll()
+    console.log(comment)
+    res.render("comment/show",{comments:comment})
+})
+
+router.post("/",async (req,res)=>{
+    console.log(req.body)
+    const comment = await db.comment.create({
+        name: req.body.userName,
+        content:req.body.comment,
+        articleId:req.body.articleId
+    })
+    res.redirect("/comment")
+})
+
+module.exports = router
