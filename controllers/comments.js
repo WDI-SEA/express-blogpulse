d993860e794ad37583a513985bd53af604691813
brandon-w1205 <bwu1205@gmail.com>
2022-09-08 22:37:38 -0700

    tries to add comment get functionality

--- controllers/comments.js
+++ controllers/comments.js
@@ -2,6 +2,18 @@ let express = require('express')
 let db = require('../models')
 let router = express.Router()
 
-
+router.post('/', (req, res) => {
+    try {
+        db.comment.create({
+            name: req.body.name,
+            content: req.body.content
+        })
+        
+    } catch(err) {
+        console.log(error)
+        res.status(400).render('main/404')
+    }  
+})
+  
 
 module.exports = router
