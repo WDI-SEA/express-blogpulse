be1d1421944ba93417334d0cc9c30406f15a4412
Agrate071818 <zionagrate@gmail.com>
2022-09-09 11:36:13 -0400

    Done

--- controllers/articles.js
+++ controllers/articles.js
@@ -29,20 +29,39 @@ router.get('/new', (req, res) => {
 })
 
 // GET /articles/:id - display a specific post and its author
+router.post('/:id', (req,res) => {
+  db.comment.create({
+    name: req.body.name,
+    content: req.body.content,
+    articleId: req.body.articleId,
+  })
+  .then((post) => {
+    res.redirect(`/articles/${req.body.articleId}`)
+  })
+  .catch((error) => {
+    res.status(400).render('main/404')
+  })
+})
+
+// GET /articles/:id - display a specific post and its author
 router.get('/:id', (req, res) => {
   db.article.findOne({
     where: { id: req.params.id },
-    include: [db.author]
+    include: [db.author, db.comment]
   })
   .then((article) => {
     if (!article) throw Error()
     console.log(article.author)
-    res.render('articles/show', { article: article })
+    console.log(article.comments)
+    console.log(article)
+    res.render('articles/show', { 
+      article: article,
+      id: req.params.id
+     })
   })
   .catch((error) => {
     console.log(error)
     res.status(400).render('main/404')
   })
 })
-
 module.exports = router
