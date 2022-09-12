4720a56a52dd8266edb2f27239b943f2b889cd19
mousesaver <sean.fan87@gmail.com>
2022-09-12 10:56:40 -0700

    Sean's homework

--- controllers/articles.js
+++ controllers/articles.js
@@ -32,12 +32,26 @@ router.get('/new', (req, res) => {
 router.get('/:id', (req, res) => {
   db.article.findOne({
     where: { id: req.params.id },
-    include: [db.author]
+    include: [db.author, db.comment]
   })
   .then((article) => {
     if (!article) throw Error()
     console.log(article.author)
     res.render('articles/show', { article: article })
+  })
+  .catch((error) => {
+    console.log(error)
+    res.status(400).render('main/404')
+  })
+})
+router.post('/:id', (req, res) => {
+  db.comment.create({
+     name: req.body.commentname,
+     content: req.body.content,
+     articleId: req.params.id
+  })
+  .then((article) => {
+    res.redirect(`/articles/${req.params.id}`);
   })
   .catch((error) => {
     console.log(error)
