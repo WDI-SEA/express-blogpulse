aa266956fc2cbbacec08187cdbe9a51057855fa3
shaikhaalbuflasa <shaikhasaad86@gmail.com>
2022-10-19 19:30:08 +0300

    model with the app with comments

--- controllers/articles.js
+++ controllers/articles.js
@@ -32,17 +32,37 @@ router.get('/new', (req, res) => {
 router.get('/:id', (req, res) => {
   db.article.findOne({
     where: { id: req.params.id },
-    include: [db.author]
+    include: [db.author, db.comment]
   })
   .then((article) => {
     if (!article) throw Error()
     console.log(article.author)
     res.render('articles/show', { article: article })
+    // res.send(article);
   })
   .catch((error) => {
     console.log(error)
     res.status(400).render('main/404')
   })
+})
+
+router.post('/:id/comments', (req,res)=>{
+  try{
+    db.comment.create({
+      name:req.body.name,
+      content:req.body.content,
+      articleId:req.params.id
+    })
+    
+    .then(newComment =>{
+      res.redirect(`/articles/${req.params.id}`)
+     
+    })
+
+  }catch(err){
+    console.log(err)
+  }
+  
 })
 
 module.exports = router
